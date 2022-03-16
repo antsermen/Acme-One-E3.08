a28613f6bcd041e52a994b406b1c9dea9fea3dfd
Jose Ramon <jrae1112jrae@gmail.com>
2022-03-16 21:32:10 +0100

    Creación Entidad PatronageReports

---
+++ src/main/java/acme/entities/PatronageReport.java
@@ -0,0 +1,61 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class PatronageReport extends AbstractEntity{
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long serialVersionUID = 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "^[0-9]{4}$")
+	protected String serialNumber;
+	
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date creationMoment;
+	
+	@NotBlank
+	@Length(max = 255)
+	protected String memorandum;
+	
+	@URL
+	protected String info;
+	
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Patronage patronage;
+	
+	// Derived attributes -----------------------------------------------------
+
+	public String sequenceNumber() {
+		return this.patronage.getCode() + ":" + this.serialNumber;
+	}
+}

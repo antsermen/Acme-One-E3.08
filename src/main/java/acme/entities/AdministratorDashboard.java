e3d27a45becb2aa1f9a2d146d2f202349ffd9d26
Roberto <antsermen@alum.us.es>
2022-03-17 19:00:12 +0100

    Finalización de la tarea 25

--- src/main/java/acme/entities/AdministratorDashboard.java
+++ src/main/java/acme/entities/AdministratorDashboard.java
@@ -48,4 +48,7 @@ public class AdministratorDashboard extends AbstractEntity {
 	Money minimumRetailPriceOfDeniedPatronages;
 	Money maximumRetailPriceOfDeniedPatronages;
 	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
 }

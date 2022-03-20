a466bd637f7087be1488fd00d041096d8be418dc
Roberto <antsermen@alum.us.es>
2022-03-20 18:38:27 +0100

    Inclusión de la entidad quantity y modificación de algunas entidades

--- src/main/java/acme/entities/Tool.java
+++ src/main/java/acme/entities/Tool.java
@@ -2,6 +2,8 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -11,6 +13,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -52,7 +55,11 @@ public class Tool extends AbstractEntity{
 	// Derived attributes -----------------------------------------------------
 	
 	// Relationships ----------------------------------------------------------
-
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Inventor inventor;
 	
 }
 

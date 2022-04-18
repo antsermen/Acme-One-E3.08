626477bdd128321e05c22374b43ab546206ffeee
Jose Ramon <jrae1112jrae@gmail.com>
2022-04-18 20:17:06 +0200

    Task 031: Controller+Services+Repository

---
+++ src/main/java/acme/features/any/userAccount/AnyUserAccountListService.java
@@ -0,0 +1,58 @@
+package acme.features.any.userAccount;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.entities.UserAccount;
+import acme.framework.roles.Any;
+import acme.framework.roles.UserRole;
+import acme.framework.services.AbstractListService;
+
+
+@Service
+public class AnyUserAccountListService implements AbstractListService<Any, UserAccount>{
+	
+	//Internal state----------------------------------------------------------------------
+	
+	@Autowired 
+	protected AnyUserAccountRepository anyUserAccountRepository;
+	
+	// AbstractListService<Any, UserAccount> interface --------------
+	@Override
+	public boolean authorise(final Request<UserAccount> request) {
+		assert request != null;
+		return true;
+	}
+
+	@Override
+	public Collection<UserAccount> findMany(final Request<UserAccount> request) {
+		assert request != null;
+		final Collection<UserAccount> result = new ArrayList<>();
+		final List<UserAccount> aux = new ArrayList<>(this.anyUserAccountRepository.findAllUserEnableds());
+		for(final UserAccount ua : aux) {
+			for(final UserRole ur : ua.getRoles()) {
+				if(!"administrator".equals(ur.getAuthorityName()) && !ua.isAnonymous()) {
+					result.add(ua);
+				}
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<UserAccount> request, final UserAccount entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+		model.setAttribute("roles", entity.getAuthorityString());
+		System.out.println(entity.getAuthorityString());
+		System.out.println(entity.getRoles());
+		request.unbind(entity, model, "username");
+	}
+}

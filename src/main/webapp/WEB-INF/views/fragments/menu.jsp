0b1a6d25ae515430562c6a4c84b1612fdcb0b1f0
JC <juamorper2@alum.us.es>
2022-04-21 12:54:52 +0200

    AREGLADO ITEMS

--- src/main/webapp/WEB-INF/views/fragments/menu.jsp
+++ src/main/webapp/WEB-INF/views/fragments/menu.jsp
@@ -31,8 +31,8 @@
 			<acme:menu-suboption code="75917728H: Sabugueiro Troya, David" action="http://www.twitter.com/"/>
 			<acme:menu-suboption code="master.menu.anonymous.chirp.list" action="/any/chirp/list"/>
 			<acme:menu-suboption code="List Toolkit" action="/any/toolkit/list"/>
-			<acme:menu-suboption code="List Tool" action="/any/tool/list"/>
-			<acme:menu-suboption code="List Component" action="/any/component/list"/>
+			<acme:menu-suboption code="master.menu.anonymous.item.tool.list" action="/any/item/tools"/>
+			<acme:menu-suboption code="master.menu.anonymous.item.component.list" action="/any/item/components"/>
 			<acme:menu-suboption code="master.menu.anonymous.userAccount.list" action="/any/user-account/list"/>
 		
       
---
+++ src/test/java/acme/testing/any/item/AnyComponentListTest.java
@@ -0,0 +1,30 @@
+package acme.testing.any.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyComponentListTest extends TestHarness{
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/item/componentList.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int recordIndex,final String name, final String code, final String technology, final String description, final String retailPrice, final String link) {
+		
+
+		super.clickOnMenu("Anonymous", "List Component");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		
+		super.checkColumnHasValue(recordIndex, 0, name);
+		super.checkColumnHasValue(recordIndex, 1, code);
+		super.checkColumnHasValue(recordIndex, 2, technology);
+		super.checkColumnHasValue(recordIndex, 3, description);
+		super.checkColumnHasValue(recordIndex, 4, retailPrice);
+		super.checkColumnHasValue(recordIndex, 5, link);
+
+	}
+
+}

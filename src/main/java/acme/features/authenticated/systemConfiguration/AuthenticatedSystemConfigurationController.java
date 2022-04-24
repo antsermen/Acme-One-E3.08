a7ea838bb09f97bcd3d7194b97d077d2f224434c
davsabtro <80279522+davsabtro@users.noreply.github.com>
2022-04-24 21:14:47 +0200

    Update AuthenticatedSystemConfigurationController.java

--- src/main/java/acme/features/authenticated/systemConfiguration/AuthenticatedSystemConfigurationController.java
+++ src/main/java/acme/features/authenticated/systemConfiguration/AuthenticatedSystemConfigurationController.java
@@ -4,9 +4,8 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.mvc.AbstractController;
-
 import acme.entities.SystemConfiguration;
+import acme.framework.controllers.AbstractController;
 import acme.framework.roles.Authenticated;
 
 @Controller

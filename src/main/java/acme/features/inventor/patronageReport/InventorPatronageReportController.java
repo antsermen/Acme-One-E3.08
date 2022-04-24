367476f96a71506e6e1e835cbacf8e19ca73df49
davsabtro <80279522+davsabtro@users.noreply.github.com>
2022-04-24 21:20:27 +0200

    Update InventorPatronageReportController.java

--- src/main/java/acme/features/inventor/patronageReport/InventorPatronageReportController.java
+++ src/main/java/acme/features/inventor/patronageReport/InventorPatronageReportController.java
@@ -4,7 +4,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.mvc.AbstractController;
+import acme.framework.controllers.AbstractController;
 
 import acme.entities.PatronageReport;
 import acme.roles.Inventor;
